6d090d3176c17e50c49839fd295448d87e99b340
Caleb Jones <code@calebjones.net>
2016-10-27 20:41:46 -0400

    Minimize the example app

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     let mut window: PistonWindow =
-        WindowSettings::new("Primitive Demo", [400, 720])
+        WindowSettings::new("Primitive Demo", [1280, 720])
         .opengl(opengl).samples(4).exit_on_esc(true).build().unwrap();
     window.set_ups(60);
 
@@ -60,34 +60,14 @@ fn main() {
 
 fn set_ui(ref mut ui: conrod::UiCell, ids: &Ids) {
     use conrod::{Positionable, Widget};
-    use conrod::widget::{Canvas, Circle, Line, Oval, PointPath, Polygon, Rectangle};
-    use std::iter::once;
+    use conrod::widget::{Canvas, PointPath};
 
     // The background canvas upon which we'll place our widgets.
     Canvas::new().pad(80.0).set(ids.canvas, ui);
 
-    Line::centred([-40.0, -40.0], [40.0, 40.0]).top_left_of(ids.canvas).set(ids.line, ui);
-
-    let left = [-40.0, -40.0];
-    let top = [0.0, 40.0];
-    let right = [40.0, -40.0];
-    let points = once(left).chain(once(top)).chain(once(right));
-    PointPath::centred(points).down(80.0).set(ids.point_path, ui);
-
-    Rectangle::fill([80.0, 80.0]).down(80.0).set(ids.rectangle_fill, ui);
-
-    Rectangle::outline([80.0, 80.0]).down(80.0).set(ids.rectangle_outline, ui);
-
-    let bl = [-40.0, -40.0];
-    let tl = [-20.0, 40.0];
-    let tr = [20.0, 40.0];
-    let br = [40.0, -40.0];
-    let points = once(bl).chain(once(tl)).chain(once(tr)).chain(once(br));
-    Polygon::centred_fill(points).right_from(ids.line, 80.0).set(ids.trapezoid, ui);
-
-    Oval::fill([40.0, 80.0]).down(80.0).align_middle_x().set(ids.oval_fill, ui);
-
-    Oval::outline([80.0, 40.0]).down(100.0).align_middle_x().set(ids.oval_outline, ui);
-
-    Circle::fill(40.0).down(100.0).align_middle_x().set(ids.circle, ui);
+    let points: Vec<_> =
+        (0u32..100).map(|t| [(t as f64/100.0).cos() * 100.0,
+                             (t as f64/100.0).sin() * 100.0])
+        .collect();
+    PointPath::centred(points).middle().set(ids.point_path, ui);
 }
